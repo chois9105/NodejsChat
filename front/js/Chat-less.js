fb077e44bb25c1be38656b17ab898d2e51946647
code1492 <xgunheex@gmail.com>
2014-02-12 00:49:35 +0900

    clean up files and update css

--- front/js/Chat-less.js
+++ front/js/Chat-less.js
@@ -186,8 +186,6 @@ $(function () {
 
   });
 
-  //////////////////////////////////////////////////////////////////////////////
-
 }) 
 
 
@@ -199,53 +197,3 @@ var move_slide  = function(num){
     });
 } 
  
-
-
-
-
-
-
- // Put event listeners into place
-window.addEventListener("DOMContentLoaded", function() {
-  // Grab elements, create settings, etc.
-  var canvas = document.getElementById("canvas"),
-    context = canvas.getContext("2d"),
-    video = document.getElementById("video"),
-    videoObj = { "video": true },
-    errBack = function(error) {
-      console.log("Video capture error: ", error.code); 
-    };
-
-  // Put video listeners into place
-  if(navigator.getUserMedia) { // Standard
-    navigator.getUserMedia(videoObj, function(stream) {
-      video.src = stream;
-      video.play();
-    }, errBack);
-  } else if(navigator.webkitGetUserMedia) { // WebKit-prefixed
-    navigator.webkitGetUserMedia(videoObj, function(stream){
-      video.src = window.webkitURL.createObjectURL(stream);
-      video.play();
-    }, errBack);
-  }
-  else if(navigator.mozGetUserMedia) { // Firefox-prefixed
-    navigator.mozGetUserMedia(videoObj, function(stream){
-      video.src = window.URL.createObjectURL(stream);
-      video.play();
-    }, errBack);
-  }
-
-  document.getElementById("snap").addEventListener("click", function() {
-    context.drawImage(video, 0, 0, 160,120);
-    video.pause();
-    video.src=null; 
-
-    video.pause();
-    video.mozSrcObject=null; 
-
-    video.pause();
-    video.src="";
-  });
-}, false);
-
-
--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 //test git 2
 var public_dir = "./front",
-    port=3000,
+    port=30000,
     total_usr_cnt=0,
     total_room_cnt=0,
     Rooms_list=[];
 
 var swig  = require('swig'),
     MongoClient = require('mongodb').MongoClient,
+    ObjectId = require('mongodb').ObjectID,
     express = require('express'),
     app = express(),
     io = require('socket.io').listen(app.listen(port));
@@ -46,23 +47,27 @@ var swig  = require('swig'),
 
 // mongodb connect
 MongoClient.connect('mongodb://localhost:27017/ChatData', function(err, db) {
-      if(err) throw err;
+      if(err){
+        console.log("DB Error. Please Check mongodb server connection. ");
+        console.log(err);
 
-      db.dropDatabase(function() { //remove collection when starting server
-      }); 
+        }
+
+      //db.dropDatabase(function() { //remove collection when starting server
+      //}); 
 
      var Room_collection = db.collection('Rooms');
      var Chat_collection = db.collection('Chats');
 
       /*collection.remove(function(err, result) { 
       });*/
-
+/*
     Room_collection.insert({"id":"1","name":"testRoom"}, function(docs) {   
         //Room_collection.count(function(err, count) {
         //}); 
     });  
     Room_collection.insert({"id":"1","name":"testRoom2"}, function(docs) { }); 
-
+*/
 
 
     // open login page
@@ -72,6 +77,7 @@ MongoClient.connect('mongodb://localhost:27017/ChatData', function(err, db) {
     });
 
     // open RoomList
+        /*
     app.post('/RoomList', function (req, res) {
         //console.log(req.body); // get post prarams 
         var RoomsArr=[];
@@ -84,10 +90,10 @@ MongoClient.connect('mongodb://localhost:27017/ChatData', function(err, db) {
         });
       //console.log(io.rooms);
     });
+        */
 
     // open Chat page
-    app.post('/Chat', function (req, res) { 
-
+    app.post('/Chat', function (req, res) {
         res.render('Chat', { 
             this_room:req.body.this_room
       });
@@ -100,6 +106,7 @@ var clsSocket={};
 
 
     io.sockets.on('connection', function (socket) { 
+        var current_room="";
 
 
         //console.log(socket.namespace.manager.rooms);
@@ -117,11 +124,45 @@ var clsSocket={};
         //socket.emit('stc', data);
         //io.sockets.in('roomf').emit('stc', data);
 
-        socket.on('ChangeRoom', function (Data) {
-            data['MyRoom'] = Data.room;
+        fnc_room_list();
+
+
+        // create Room
+        socket.on('CreateRoom', function (CreateRoom_Data) {  
+/*
+            Room_collection.find(Data).toArray(function(err, rows) { 
+                //res.send('<script>location.href="/Chat";</script>');  
+                console.log(rows);
+            });
+*/
+            Room_collection.insert(CreateRoom_Data, function(err,rec) {  
+                console.log(rec._id);
+                //get Room list from mongoDB
+                Room_collection.find().toArray(function(err, rows) { 
+                    io.sockets.emit('send_room_list', { 
+                        room_list: rows
+                    }); 
+                }); 
+            }); 
+        });
+
+
+        // change Room
+        socket.on('ChangeRoom', function (ChangeRoom_Data) { 
+            current_room = ChangeRoom_Data.room;
             //join room
             socket.leave(null);
-            socket.join(data['MyRoom']);
+            socket.join(current_room); 
+            //get room's clients
+            fnc_user_list();
+
+        });
+
+
+        // change Room
+        socket.on('ctl', function (Loc_Data) { 
+            socket.emit('stlm', Loc_Data);
+            socket.broadcast.to(current_room).emit('stl', Loc_Data);
         });
 
 
@@ -129,6 +170,7 @@ var clsSocket={};
         socket.on('ctsSetNick', function (nameData) {
             //clients nicknames
             socket.nickname = nameData.nick;
+            console.log('asdf'+socket.nickname);
             clsSocket[myid] = socket; 
 
             console.log("join : " + clsSocket[myid].nickname);
@@ -143,47 +185,116 @@ var clsSocket={};
             });
             // send clients Array 
             //io.sockets.sockets[""].emit('usrs', cls); //emit close
-            io.sockets.in(data['MyRoom']).emit('usrs', cls); //emit close
+            //io.sockets.in(data['MyRoom']).emit('usrs', cls); //emit close
             console.log(cls);
 
             console.log('---------------------------');
             console.log(socket.namespace.manager.rooms);
-        });
+        }); 
+ 
 
         // client to server data
         socket.on('cts', function (client_data) {
-            console.log(data['MyRoom']+"____");
+            console.log(current_room+" only ");
             //send msg
-            //socket.broadcast.to('room').emit('stc',data);
-            io.sockets.in(data['MyRoom']).emit('stc', client_data);
-            //to broadcast information to a certain room (excluding the client):
-            //socket.broadcast.to('room1').emit('function', 'data1', 'data2');
-            //to broadcast information globally:
-            //io.sockets.in('room1').emit('function', 'data1', 'data2');
+            //only room
+            //io.sockets.in(data['MyRoom']).emit('stc', client_data);
+            //to others
+            socket.broadcast.to(current_room).emit('stc', client_data);
+            //only me 
+            socket.emit('stm', client_data);
         });
 
 
         // client to server data
         socket.on('ctp', function (client_pdata) {
             //send msg 
-            socket.emit('stc', client_pdata);
+            socket.emit('stm', client_pdata);
             io.sockets.sockets[client_pdata.private].emit('stc', client_pdata);
-     
+        });
+
+        // leave room
+        socket.on('leaveRoom', function () {
+            disonnect();
+
         });
 
         socket.on('disconnect', function () {
-            socket.leave(data['MyRoom']);
+            disonnect();
+        });
+
+
+
+        function fnc_room_list(){  
+        //get Room list from mongoDB
+            Room_collection.find().toArray(function(err, rows) { 
+                io.sockets.emit('send_room_list', { 
+                    room_list: rows
+                }); 
+            }); 
+        };
+
+        function fnc_user_list(uid){
+            if(uid != "")
+            delete clsSocket[uid]; 
+
+            //get room's clients
+            var cls = [];
+            var roster = io.sockets.clients(current_room);
+
+            roster.forEach(function (client) {
+                //cls.push("{'nick':'"+client.nickname+"'}");
+                cls.push({ 'id': client.id, 'nick': client.nickname });
+            });
+            // send clients Array 
+            //io.sockets.sockets[""].emit('usrs', cls); //emit close
+            io.sockets.in(current_room).emit('usrs', cls); //emit close
+            console.log(cls);
+
+            console.log('---------------------------');
+            console.log(socket.namespace.manager.rooms); 
+            return cls;
+        };
+
+        function disonnect(){
+            socket.leave(current_room);
+            console.log("leave : "+current_room);
+            /*
             delete clsSocket[socket.id];
             //get room's clients
             var cls = [];
-            var roster = io.sockets.clients(data['MyRoom']);
-            roster.forEach(function (client) {
+            var roster = io.sockets.clients(current_room);
+            roster.forEach(function (client) { 
                 cls.push({ 'id': client.id, 'nick': client.nickname });
-            });
-            console.log(cls);
-            // send clients Array
-            io.sockets.in(data['MyRoom']).emit('usrs', cls); //emit close
-        });
+            }); */
+            var cls = fnc_user_list(socket.id);
+            if(!current_room){
+                console.log("Not joined user out.");
+            }else{
+                // if room of no user
+                if(cls.length<1){ 
+
+                    //then remove room where mongodb
+                    //Room_collection.remove({"_id":{ '$in': [ ObjectId("current_room") ] }}, function(docs) {   
+                    Room_collection.remove({'_id': { '$in': [ObjectId(current_room)] }}, function(err,docs) {   
+                        console.log("remove err : " + err);
+                        console.log("remove : " + docs);
+                        //Room_collection.count(function(err, count) {
+                        //});  
+                        fnc_room_list();
+                    }); 
+                }else{
+                // send clients Array
+                io.sockets.in(current_room).emit('usrs', cls); //emit close
+               }
+            }
+        };
 
     }) //io.sockets.on('connection', function (socket) {
 }); // mongodb connection close
+
+
+
+
+
+
