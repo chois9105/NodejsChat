d0011bada48bcc4531b7aa4069b0c5a0a4616a58
chois9105 <chois9105@gmail.com>
2014-04-04 23:38:08 +0900

    chat enabled
    chatting

--- front/js/Chat-less.js
+++ front/js/Chat-less.js
@@ -1,56 +1,18 @@
 
-
-
-function resize_chat(){
-  var win = $(window);
-      fr = $("#frames");
-      //fr.css({"-webkit-transform":"translate("+(win.width()/2-fr.width()/2)+"px,"+(win.height()/2-fr.height()/2)+"px) scale("+win.width()/fr.width()+","+win.height()/fr.height()+")"});
-      // horizental resize 
-
-
-
-
-      if(win.width()/fr.width() > 1.3){
-        fr.css({"left":win.width()/2-fr.width()/2,
-          "transform":"scale("+1.3+","+1.3+")",
-          "-ms-transform":"scale("+1.3+","+1.3+")",
-          "-webkit-transform":"scale("+1.3+","+1.3+")"
-        }).css({"top":win.height()/2-fr.height()/2});
-      }else{
-        fr.css({"left":win.width()/2-fr.width()/2,
-        "transform":"scale("+win.width()/fr.width()+","+win.width()/fr.width()+")",
-        "-ms-transform":"scale("+win.width()/fr.width()+","+win.width()/fr.width()+")",
-        "-webkit-transform":"scale("+win.width()/fr.width()+","+win.width()/fr.width()+")"
-      }).css({"top":win.height()/2-fr.height()/2});
-      }
- 
- 
-    
-}
-
-$(window).resize(function(){
-resize_chat();
-});
-
-
 $(function () {
 
-  resize_chat();
+
  var socket = io.connect("http://"+location.host);
 
   $(document).on('click touchstart','.Room',function(){ 
     var data = {"room":$(this).attr("id")};
     socket.emit('ChangeRoom', data);
 
-    move_slide('next'); 
+     // transition will generate bug if RoomName is focused
+     setTimeout(function(){
+        move_slide('next'); 
+     },100);
     $("#area_chat").html("");
-
-  });
-
-  $("#btn_disconnect").click(function(){ 
-    var data = {"data":"data"};
-    socket.emit('leaveRoom');
-    move_slide(1);
   });
 
 // login btn 
@@ -63,18 +25,14 @@ $(function () {
         return;
       }
 
- 
       $("#myid").html(nick);
       socket.emit('ctsSetNick', { nick: nick }); 
-  
-      move_slide('next'); 
-    });
 
-    // toggle room name box 
-    $("#toggle_room_box, p[name='toggle_room_box']").click(function (){
-      $("#RoomName").val("");
-      $("div[name='create_room_text'],div[name='create_room_box']").toggle();
-      $("#RoomName").focus();
+     // transition will generate bug if txt_nick is focused  
+      setTimeout(function(){
+        move_slide('next');
+      },100);
+      
     });
 
     // add room btn
@@ -85,18 +43,8 @@ $(function () {
       } 
       var RoomData = {"id":"1","name":$("#RoomName").val()};
       socket.emit('CreateRoom', RoomData);
-      $("#toggle_room_box, p[name='toggle_room_box']").click();
     });
 
-
-//when connected...
-  socket.on('con', function (data) {
-  	//if(data.stat == 'connected') 
-
-    $("#frames").show('slow');
-
-    $("#myid").css("color","green");
-  }); 
 
   socket.on('send_room_list', function (data) { 
     //if(data.stat == 'connected') 
@@ -105,6 +53,7 @@ $(function () {
     for(var i=0,j=data.room_list.length;i<j;i++){
       $("#RoomList").append("<a class='Room list-group-item' id='"+data.room_list[i]._id+"'>"+data.room_list[i].name+"</a>");
     }
+
   }); 
 
 
@@ -123,15 +72,37 @@ $(function () {
 
 //get msg
     socket.on('stc', function (client_data) {
-      $("#area_chat").append('<tr><td><div class="message-box"><div class="picture left"><img src="https://lh3.googleusercontent.com/-QDJ4kAyQVz0/AAAAAAAAAAI/AAAAAAAAAAA/M7wANYrPEmE/s46-c-k-no/photo.jpg" ><span class="time"></span></div><div class="message-left"><span style="display:block;">'+client_data.nick+'</span><pre>'+client_data.msg+'</pre></div></div></td></tr>');
-    	$(".area_chat_div").scrollTo("110%",180);
+
+      var receivedTime = '<small class="pull-right text-muted"><span class="glyphicon glyphicon-time"></span> 12 mins ago </small>'; 
+      var receivedMsg = '<p>' + client_data.msg + '</p>'
+      var chatHeader = '<div class="header"> <strong class="primary-font">' + client_data.nick + '</strong>' + receivedTime + '</header>';
+      var chatHeaderWrapper = '<div class="chat-body clearfix">' +  chatHeader + receivedMsg + '</div>'
+      var chatUserImg = '<span class="chat-img pull-left"><img src="http://placehold.it/50/55C1E7/fff&amp;text=U" alt="User Avatar" class="img-circle"> </span>';
+      var msgWrapper = '<li class="left clearfix">' + chatUserImg + chatHeaderWrapper + '</li>';
+
+      $('#area_chat').append(msgWrapper);
+      $(".area_chat_div").scrollTo("110%",180);
+
     });
 
+
+
     socket.on('stm', function (client_data) {
-      $("#area_chat").append('<tr><td><div class="asdf"><div class="message-box right"><div class="picture right"><img src="https://lh3.googleusercontent.com/-QDJ4kAyQVz0/AAAAAAAAAAI/AAAAAAAAAAA/M7wANYrPEmE/s46-c-k-no/photo.jpg" ><span class="time"></span></div><div class="message-right"><span style="display:block;">'+client_data.nick+'</span><pre>'+client_data.msg+'</pre></div></div></div></td></tr>');
+
+      console.log(client_data);
+
+      var receivedTime = '<small class="text-muted"><span class="glyphicon glyphicon-time"></span> 13 mins ago </small>'; 
+      var receivedMsg = '<p>' + client_data.msg + '</p>'
+      var chatHeader = '<div class="header">' + receivedTime + '<strong class="pull-right primary-font">' + client_data.nick + '</strong></header>';
+      var chatHeaderWrapper = '<div class="chat-body clearfix">' +  chatHeader + receivedMsg + '</div>'
+      var chatUserImg = '<span class="chat-img pull-right"><img src="http://placehold.it/50/FA6F57/fff&amp;text=ME" alt="User Avatar" class="img-circle"></span>';
+      var msgWrapper = '<li class="right clearfix">' + chatUserImg + chatHeaderWrapper + '</li>';
+
+      $('#area_chat').append(msgWrapper);
       $(".area_chat_div").scrollTo("110%",180);
     });
  
+
     socket.on('stl', function (client_data) {
       var img_src = "http://maps.googleapis.com/maps/api/staticmap?center="+client_data.lat+","+client_data.long+"&zoom=18&size=200x200&sensor=false&markers="+client_data.lat+","+client_data.long;
 
@@ -200,7 +171,6 @@ $(function () {
 }) 
 
 var move_slide = function(direction){
-
   if(direction == 'next'){
     $('a.right').click();    
   }else if(direction == 'prev'){
