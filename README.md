061b811f778974f228bd1ffa1effa26ae96b997d
chois9105 <chois9105@gmail.com>
2014-06-14 22:52:49 +0900

    소스정
    주석 및 오픈쉬프트관련 내용 삭제

--- README.md
+++ README.md
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
-NodejsChat
-==========
-
 
 NodejsChat 
 
@@ -23,8 +19,9 @@ less.js
 
 
 DEMO:  http://geeky.kr:30000/less
-=======
-The OpenShift `nodejs` cartridge documentation can be found at:
 
-https://github.com/openshift/origin-server/tree/master/cartridges/openshift-origin-cartridge-nodejs/README.md
->>>>>>> 655dcdb208b510413deede270664ca27fc2a0e64
+--------------------------------------------------------------------------------
+First : mongod 
+Second : mongo
+Third : node server.js
+
--- server.js
+++ server.js
@@ -53,38 +53,12 @@ MongoClient.connect('mongodb://localhost:27017/ChatData', function(err, db) {
      var Room_collection = db.collection('Rooms');
      var Chat_collection = db.collection('Chats');
 
-      /*collection.remove(function(err, result) { 
-      });*/
-/*
-    Room_collection.insert({"id":"1","name":"testRoom"}, function(docs) {   
-        //Room_collection.count(function(err, count) {
-        //}); 
-    });  
-    Room_collection.insert({"id":"1","name":"testRoom2"}, function(docs) { }); 
-*/
-
 
     // open login page
     app.get('/login', function (req, res) {
       res.render('login', { 
       });
     });
-
-    // open RoomList
-        /*
-    app.post('/RoomList', function (req, res) {
-        //console.log(req.body); // get post prarams 
-        var RoomsArr=[];
-        //get Room list from mongoDB
-        Room_collection.find().toArray(function(err, rows) { 
-            //res.send('<script>location.href="/Chat";</script>');
-            res.render('RoomList', { 
-                rooms: rows
-            });
-        });
-      //console.log(io.rooms);
-    });
-        */
 
     // open Chat page
     app.post('/Chat', function (req, res) {
@@ -101,10 +75,6 @@ var clsSocket={};
 
     io.sockets.on('connection', function (socket) { 
         var current_room="";
-
-
-        //console.log(socket.namespace.manager.rooms);
-        //console.log(io.sockets.in('roomf').manager.rooms);
 
         var myid = socket.id;
 
@@ -127,12 +97,7 @@ var clsSocket={};
 
         // create Room
         socket.on('CreateRoom', function (CreateRoom_Data) {  
-/*
-            Room_collection.find(Data).toArray(function(err, rows) { 
-                //res.send('<script>location.href="/Chat";</script>');  
-                console.log(rows);
-            });
-*/
+
             Room_collection.insert(CreateRoom_Data, function(err,rec) {  
                 console.log(rec._id);
                 //get Room list from mongoDB
@@ -159,6 +124,10 @@ var clsSocket={};
 
         // change Room
         socket.on('ctl', function (Loc_Data) { 
+
+            console.log("ctl")
+            console.log(Loc_Data);
+
             socket.emit('stlm', Loc_Data);
             socket.broadcast.to(current_room).emit('stl', Loc_Data);
         });
@@ -181,9 +150,8 @@ var clsSocket={};
                 //cls.push("{'nick':'"+client.nickname+"'}");
                 cls.push({ 'id': client.id, 'nick': client.nickname });
             });
-            // send clients Array 
-            //io.sockets.sockets[""].emit('usrs', cls); //emit close
-            //io.sockets.in(data['MyRoom']).emit('usrs', cls); //emit close
+
+
             console.log(cls);
 
             console.log('---------------------------');
@@ -194,12 +162,13 @@ var clsSocket={};
         // client to server data
         socket.on('cts', function (client_data) {
             console.log(current_room+" only ");
-            //send msg
-            //only room
-            //io.sockets.in(data['MyRoom']).emit('stc', client_data);
-            //to others
+
             socket.broadcast.to(current_room).emit('stc', client_data);
-            //only me 
+            //only me
+
+            console.log("client_data");
+            console.log(client_data);
+
             socket.emit('stm', client_data);
         });
 
@@ -207,6 +176,11 @@ var clsSocket={};
         // client to server data
         socket.on('ctp', function (client_pdata) {
             //send msg 
+
+
+            console.log("client_pdata");
+            console.log(client_pdata);
+
             socket.emit('stm', client_pdata);
             io.sockets.sockets[client_pdata.private].emit('stc', client_pdata);
         });
@@ -257,14 +231,7 @@ var clsSocket={};
         function disonnect(){
             socket.leave(current_room);
             console.log("leave : "+current_room);
-            /*
-            delete clsSocket[socket.id];
-            //get room's clients
-            var cls = [];
-            var roster = io.sockets.clients(current_room);
-            roster.forEach(function (client) { 
-                cls.push({ 'id': client.id, 'nick': client.nickname });
-            }); */
+
             var cls = fnc_user_list(socket.id);
             if(!current_room){
                 console.log("Not joined user out.");
@@ -291,162 +258,4 @@ var clsSocket={};
     }) //io.sockets.on('connection', function (socket) {
 }); // mongodb connection close
 
-
-//  OpenShift sample Node application
-var express = require('express');
-var fs      = require('fs');
-
-
-/**
- *  Define the sample application.
- */
-var SampleApp = function() {
-
-    //  Scope.
-    var self = this;
-
-
-    /*  ================================================================  */
-    /*  Helper functions.                                                 */
-    /*  ================================================================  */
-
-    /**
-     *  Set up server IP address and port # using env variables/defaults.
-     */
-    self.setupVariables = function() {
-        //  Set the environment variables we need.
-        self.ipaddress = process.env.OPENSHIFT_NODEJS_IP;
-        self.port      = process.env.OPENSHIFT_NODEJS_PORT || 8080;
-
-        if (typeof self.ipaddress === "undefined") {
-            //  Log errors on OpenShift but continue w/ 127.0.0.1 - this
-            //  allows us to run/test the app locally.
-            console.warn('No OPENSHIFT_NODEJS_IP var, using 127.0.0.1');
-            self.ipaddress = "127.0.0.1";
-        };
-    };
-
-
-    /**
-     *  Populate the cache.
-     */
-    self.populateCache = function() {
-        if (typeof self.zcache === "undefined") {
-            self.zcache = { 'index.html': '' };
-        }
-
-        //  Local cache for static content.
-        self.zcache['index.html'] = fs.readFileSync('./index.html');
-    };
-
-
-    /**
-     *  Retrieve entry (content) from cache.
-     *  @param {string} key  Key identifying content to retrieve from cache.
-     */
-    self.cache_get = function(key) { return self.zcache[key]; };
-
-
-    /**
-     *  terminator === the termination handler
-     *  Terminate server on receipt of the specified signal.
-     *  @param {string} sig  Signal to terminate on.
-     */
-    self.terminator = function(sig){
-        if (typeof sig === "string") {
-           console.log('%s: Received %s - terminating sample app ...',
-                       Date(Date.now()), sig);
-           process.exit(1);
-        }
-        console.log('%s: Node server stopped.', Date(Date.now()) );
-    };
-
-
-    /**
-     *  Setup termination handlers (for exit and a list of signals).
-     */
-    self.setupTerminationHandlers = function(){
-        //  Process on exit and signals.
-        process.on('exit', function() { self.terminator(); });
-
-        // Removed 'SIGPIPE' from the list - bugz 852598.
-        ['SIGHUP', 'SIGINT', 'SIGQUIT', 'SIGILL', 'SIGTRAP', 'SIGABRT',
-         'SIGBUS', 'SIGFPE', 'SIGUSR1', 'SIGSEGV', 'SIGUSR2', 'SIGTERM'
-        ].forEach(function(element, index, array) {
-            process.on(element, function() { self.terminator(element); });
-        });
-    };
-
-
-    /*  ================================================================  */
-    /*  App server functions (main app logic here).                       */
-    /*  ================================================================  */
-
-    /**
-     *  Create the routing table entries + handlers for the application.
-     */
-    self.createRoutes = function() {
-        self.routes = { };
-
-        self.routes['/asciimo'] = function(req, res) {
-            var link = "http://i.imgur.com/kmbjB.png";
-            res.send("<html><body><img src='" + link + "'></body></html>");
-        };
-
-        self.routes['/'] = function(req, res) {
-            res.setHeader('Content-Type', 'text/html');
-            res.send(self.cache_get('index.html') );
-        };
-    };
-
-
-    /**
-     *  Initialize the server (express) and create the routes and register
-     *  the handlers.
-     */
-    self.initializeServer = function() {
-        self.createRoutes();
-        self.app = express.createServer();
-
-        //  Add handlers for the app (from the routes).
-        for (var r in self.routes) {
-            self.app.get(r, self.routes[r]);
-        }
-    };
-
-
-    /**
-     *  Initializes the sample application.
-     */
-    self.initialize = function() {
-        self.setupVariables();
-        self.populateCache();
-        self.setupTerminationHandlers();
-
-        // Create the express server and routes.
-        self.initializeServer();
-    };
-
-
-    /**
-     *  Start the server (starts up the sample application).
-     */
-    self.start = function() {
-        //  Start the app on the specific interface (and port).
-        self.app.listen(self.port, self.ipaddress, function() {
-            console.log('%s: Node server started on %s:%d ...',
-                        Date(Date.now() ), self.ipaddress, self.port);
-        });
-    };
-
-};   /*  Sample Application.  */
-
-
-
-/**
- *  main():  Main code.
- */
-var zapp = new SampleApp();
-zapp.initialize();
-zapp.start();
 
